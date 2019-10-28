ebf2dbf7478060303269c78646867f621c2e1d56
JPlante9117 <jacrplante@gmail.com>
2019-10-28 09:25:34 -0700

    cleaned newgame url finder

--- lib/gamestop_scraper/scraper.rb
+++ lib/gamestop_scraper/scraper.rb
@@ -7,13 +7,29 @@ class GamestopScraper::Scraper
         doc.css("div.row.key-art-carousel")[0].css(".slide").each do |game|
             #binding.pry
             new_game = GamestopScraper::Game.new
-            new_game.url = game.css("a").attribute("href").value
             new_game.title = game.css("a").css(".font-bold").text.strip
+            new_game.url = game.css("a").attribute("href").value
+            unless new_game.url.include?("/video-games/")
+                if new_game.url.include?("/collection/")
+                    url_name = new_game.title.gsub(/[.,\/#!$%\^&\*;:{}=\-_`~()]/,"").gsub(" ", "+")
+                    html = open("https://www.gamestop.com/search/?q=#{url_name}&lang=default")
+                    doc = Nokogiri::HTML(html)
+                    new_game.url = doc.css(".tab-pane .row .product-grid-wrapper .row .product-grid-tile-wrapper")[0].css("a").attribute("href").value
+                end
+                if new_game.url.include?("/video-games?")
+                    html = open("https://www.gamestop.com#{new_game.url}")
+                    doc = Nokogiri::HTML(html)
+                    new_game.url = doc.css(".tab-pane .row .product-grid-wrapper .row .product-grid-tile-wrapper")[0].css("a").attribute("href").value
+                end
+               
+            end
+            
             GamestopScraper::Game.new_games << new_game
         end
-
+        
+        html = open("https://www.gamestop.com/")
+        doc = Nokogiri::HTML(html)
         doc.css("div.row.key-art-carousel")[1].css(".slide").each do |game|
-            #binding.pry
             upcoming_release = GamestopScraper::Game.new
             upcoming_release.url = game.css("a").attribute("href").value
             upcoming_release.title = game.css("a").css(".font-bold").text.strip
@@ -22,10 +38,14 @@ class GamestopScraper::Scraper
 
     end
 
+    def url_corrector(game)
+       
+    end
+
     def self.scrape_game_details(game)
         html = open("https://www.gamestop.com#{game.url}")
         doc = Nokogiri::HTML(html)
-        binding.pry
+        #binding.pry
         game.publisher = doc.css(".product-publisher .pr-1")[1].text.strip
         game.rating = doc.css("div.tab-pane").children[13].children[5].children[3].children[0].text.strip
         game.platforms = []
